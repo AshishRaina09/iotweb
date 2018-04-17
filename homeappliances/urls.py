fbaf67311def27e6c7a58b76fc69a60961dd44fc
AshishRaina09 <ashuraina09@gmail.com>
2018-04-17 15:46:47 +0530

    change name

--- homeappliances/urls.py
+++ homeappliances/urls.py
@@ -8,7 +8,7 @@ app_name = 'homeappliances'
 
 urlpatterns = [
     path('ledbulb',  LED_bulbList.as_view(), name = 'ledbulb'),
-	path('exhaust_fan',  LED_bulbList.as_view(), name = 'ledbulb'),
+	path('exhaust_fan',  Exhaus_FanList.as_view(), name = 'ledbulb'),
     
 
 ]
